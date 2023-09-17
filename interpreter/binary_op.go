a794d33b436f7164f47acd2fa3986690518e708d
Gabriel Hernandes <ghh.hernandes@gmail.com>
2023-09-17 14:50:03 -0300

    better switching

--- interpreter/binary_op.go
+++ interpreter/binary_op.go
@@ -21,57 +21,57 @@ func (i interpreter) eq(l, r ast.Term) ast.Term {
 }
 
 func (i interpreter) neq(l, r ast.Term) ast.Term {
-	switch l.(type) {
+	switch n := l.(type) {
 	case ast.Int:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Int).Value != r.(ast.Int).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value != r.(ast.Int).Value}
 	case ast.Str:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Str).Value != r.(ast.Str).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value != r.(ast.Str).Value}
 	case ast.Bool:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Bool).Value != r.(ast.Bool).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value != r.(ast.Bool).Value}
 	default:
 		return nil
 	}
 }
 
 func (i interpreter) lt(l, r ast.Term) ast.Term {
-	switch l.(type) {
+	switch n := l.(type) {
 	case ast.Int:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Int).Value < r.(ast.Int).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value < r.(ast.Int).Value}
 	case ast.Str:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Str).Value < r.(ast.Str).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value < r.(ast.Str).Value}
 	default:
 		return nil
 	}
 }
 
 func (i interpreter) lte(l, r ast.Term) ast.Term {
-	switch l.(type) {
+	switch n := l.(type) {
 	case ast.Int:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Int).Value <= r.(ast.Int).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value <= r.(ast.Int).Value}
 	case ast.Str:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Str).Value <= r.(ast.Str).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value <= r.(ast.Str).Value}
 	default:
 		return nil
 	}
 }
 
 func (i interpreter) gt(l, r ast.Term) ast.Term {
-	switch l.(type) {
+	switch n := l.(type) {
 	case ast.Int:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Int).Value > r.(ast.Int).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value > r.(ast.Int).Value}
 	case ast.Str:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Str).Value > r.(ast.Str).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value > r.(ast.Str).Value}
 	default:
 		return nil
 	}
 }
 
 func (i interpreter) gte(l, r ast.Term) ast.Term {
-	switch l.(type) {
+	switch n := l.(type) {
 	case ast.Int:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Int).Value >= r.(ast.Int).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value >= r.(ast.Int).Value}
 	case ast.Str:
-		return ast.Bool{Kind: ast.BOOL, Value: l.(ast.Str).Value >= r.(ast.Str).Value}
+		return ast.Bool{Kind: ast.BOOL, Value: n.Value >= r.(ast.Str).Value}
 	default:
 		return nil
 	}
